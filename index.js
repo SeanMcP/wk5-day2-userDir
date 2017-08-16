6925e3a14d50ec18678c3d916cf601fec26a0b61
Sean McPherson <seanmcp25@gmail.com>
2017-08-16 10:00:56 -0400

    Remove iteration on listing page

--- index.js
+++ index.js
@@ -13,12 +13,10 @@ app.get('/', function(req, res){
   res.render('directory', { userData: data.users});
 });
 app.get('/listing/:id', function(req, res){
-
   let singleUser = data.users.find(function(user){
     return user.id == req.params.id;
   });
-
-  res.render('listing', { userData: singleUser});
+  res.render('listing', singleUser);
 });
 
 app.listen(3000, function(){
