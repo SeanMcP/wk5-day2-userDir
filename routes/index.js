ec9b4ff41d9f12790ce2dc2d27d120bb43436468
Sean McPherson <seanmcp25@gmail.com>
2017-08-22 15:39:40 -0400

    Add search employed function; css missing on listing page

--- routes/index.js
+++ routes/index.js
@@ -29,8 +29,56 @@ const getListings = function(req, res, next) {
   }
 }
 
+const getAvailable = function(req, res, next) {
+  let MongoClient = require('mongodb').MongoClient;
+  let assert = require('assert');
+
+  let url = 'mongodb://localhost:27017/users';
+
+  MongoClient.connect(url, function(err, db){
+    assert.equal(null, err);
+
+    getData(db, function() {
+      db.close();
+      next();
+    });
+  });
+  let getData = function(db, callback) {
+    let users = db.collection('users');
+
+    users.find({'job': null}).toArray().then(function(users) {
+        data = users;
+        callback();
+    });
+  }
+}
+
+const getEmployed = function(req, res, next) {
+  let MongoClient = require('mongodb').MongoClient;
+  let assert = require('assert');
+
+  let url = 'mongodb://localhost:27017/users';
+
+  MongoClient.connect(url, function(err, db){
+    assert.equal(null, err);
+
+    getData(db, function() {
+      db.close();
+      next();
+    });
+  });
+  let getData = function(db, callback) {
+    let users = db.collection('users');
+
+    users.find({'job': {$ne:null}}).toArray().then(function(users) {
+        data = users;
+        callback();
+    });
+  }
+}
+
 router.get('/', getListings, function(req, res) {
-  res.render('directory', { userData: data});
+  res.render('results', { userData: data});
 });
 router.get('/listing/:id', getListings, function(req, res){
   let singleUser = data.find(function(user){
@@ -38,5 +86,13 @@ router.get('/listing/:id', getListings, function(req, res){
   });
   res.render('listing', singleUser);
 });
+
+router.get('/available', getAvailable, function(req, res) {
+  res.render('results', { userData: data});
+})
+
+router.get('/employed', getEmployed, function(req, res) {
+  res.render('results', { userData: data});
+})
 
 module.exports = router;
